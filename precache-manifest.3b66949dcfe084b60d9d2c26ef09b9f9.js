4cc274d9973e82d3ae8a4dc0b956fcc89bf051f2
Adam J <aj@adamdcjones.com>
2020-08-15 13:56:01 -0400

    Updates

---
+++ precache-manifest.3b66949dcfe084b60d9d2c26ef09b9f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba5eab9272f6734e7b5c869bf1e74bf0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "13650af93c18a870995a",
+    "url": "/monsters-rolodex/static/css/main.e1a698e2.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13650af93c18a870995a",
+    "url": "/monsters-rolodex/static/js/main.100b580c.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
